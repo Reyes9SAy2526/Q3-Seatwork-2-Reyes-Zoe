7bd496c9ebda798ce6b67fdd1fa6cc2b41c8a41c
Reyes9SAy2526 <reyes.zs@obmontessori.edu.ph>
2026-03-03 18:33:03 +0800

    main.js

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-function checkLocation(){
-    let budget= document.getElementById("budget").value;
-    let discount = budget * .2;
+function checkDiscount() {
+
+    let budget = document.getElementById("budget").value;
+    let discount = budget * 0.20;
+
+    document.getElementById("discount").innerHTML =
+        "Eligible Discount: " + discount;
+}
+
+function checkLocation() {
+    
+    document.getElementById("location").innerHTML =
+        "Available at VillMan branches near you.";
 }
